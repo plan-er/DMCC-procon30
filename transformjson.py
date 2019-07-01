e9df6a98ce8b646d0c689e3c025d45172b949817
plan-er <kohashi1100@gmail.com>
2019-07-01 18:19:18 +0900

    クラス化

--- transformjson.py
+++ transformjson.py
@@ -1,14 +1,15 @@
 import json
 
-def r_transform(path_receive):
-    receive = open(path_receive,'r')
-    transformed = json.load(receive)
-    return transformed
+class Transform:
+    def r_transform(self,path_receive):
+        receive = open(path_receive,'r')
+        transformed = json.load(receive)
+        return transformed
 
-def t_transform(path_transmit,actions):
-    with open(path_transmit,'w') as f:
-        json.dump(actions,f,indent=4)                
+    def t_transform(self,path_transmit,actions):
+        with open(path_transmit,'w') as f:
+            json.dump(actions,f,indent=4)
 
 if __name__ == "__main__":
-    print(r_transform('フィールド情報_turn0.json'))
-    print(r_transform('フィールド情報_turn1.json'))
+    print(Transform.r_transform(None,'フィールド情報_turn0.json'))
+    print(Transform.r_transform(None,'フィールド情報_turn1.json'))
