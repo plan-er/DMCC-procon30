c30dd946786be24cbe3a0213bb79e014ff346680
plan-er <kohashi1100@gmail.com>
2019-07-04 17:52:56 +0900

    テストのための部分を追加

--- transformjson.py
+++ transformjson.py
@@ -1,15 +1,18 @@
 import json
 
-class Transform:
-    def r_transform(self,path_receive):
-        receive = open(path_receive,'r')
-        transformed = json.load(receive)
-        return transformed
+def r_transform(path_receive):
+    receive = open(path_receive,'r')
+    transformed = json.load(receive)
+    return transformed
 
-    def t_transform(self,path_transmit,actions):
-        with open(path_transmit,'w') as f:
-            json.dump(actions,f,indent=4)
+def t_transform(path_transmit,actions):
+    with open(path_transmit,'w') as f:
+        json.dump(actions,f,indent=4)
+    return f
 
 if __name__ == "__main__":
-    print(Transform.r_transform(None,'フィールド情報_turn0.json'))
-    print(Transform.r_transform(None,'フィールド情報_turn1.json'))
+    print(r_transform('フィールド情報_turn0.json'))
+    print(r_transform('フィールド情報_turn1.json'))
+    t_transform('行動情報_ID0.json',r_transform('行動情報_ID5.json'))
+    with open('行動情報_ID0.json') as f:
+        print(f.read())
