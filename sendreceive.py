0eefb5c9d0a68619e8035b6d8de35e9811f4f5a9
plan-er <kohashi1100@gmail.com>
2019-08-09 12:29:40 +0900

    テスト用項目の追加

--- sendreceive.py
+++ sendreceive.py
@@ -8,10 +8,33 @@ def getField(url, token):
         html = res.read().decode("utf-8")
         return html
 
-#def sendAction(url, token, action):
+def sendAction(url, token, action):
+    req = urllib.request.Request(url, action, token)
+    with urllib.request.urlopen(req) as res:
+        html = res.read().decode("utf-8")
+        return html
 
 if __name__ == "__main__":
     sampleToken = {
         "Authorization": "procon30_example_token"
     }
-    print(getField("http://localhost:8081/ping", sampleToken))
+    url = "http://localhost:8081/ping"
+    print(getField(url, sampleToken))
+    
+    sampleAction = {
+        "actions": [
+                {
+                    "agentID": 9,
+                    "type": "move",
+                    "dx": 1,
+                    "dy": 1
+                },
+                {
+                    "agentID": 10,
+                    "type": "move",
+                    "dx": -1,
+                    "dy": -1
+                }
+            ]
+    }
+    print(sendAction(url, sampleToken, sampleAction))
